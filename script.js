e423680c0585de905fe3377dd51c9a0aa063ac1a
Javier <javier.medina15@gmail.com>
2022-10-26 23:13:05 -0400

    Add the Book constructor and prototype function

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-console.log('test');
+//Library
+let myLibrary = [];
+
+console.log(myLibrary);
+
+//Constructor
+function Book(title, author, pages, read) {
+  this.title = title
+  this.author = author
+  this.pages = pages
+  this.read = read
+}
+
+Book.prototype.info = function() {
+  return this.title + ' by ' + this.author + ', ' + this.pages + ', ' + this.read;
+}
+
+const theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', '295 pages', 'not read yet')
+
+console.log(theHobbit.info());
+
+//Book addition function
+function addBookToLibrary() {
+  //function
+}
