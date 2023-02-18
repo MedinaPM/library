a9c75f811ebe63467287243dfc33f14a68b52c2d
Javier <javier.medina15@gmail.com>
2023-02-17 23:12:02 -0500

    Add a function that can take user’s input

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-//Library
+// Library
 let myLibrary = [];
 
 console.log(myLibrary);
 
-//Constructor
+// Constructor
 function Book(title, author, pages, read) {
   this.title = title
   this.author = author
@@ -11,15 +11,23 @@ function Book(title, author, pages, read) {
   this.read = read
 }
 
-Book.prototype.info = function() {
-  return this.title + ' by ' + this.author + ', ' + this.pages + ', ' + this.read;
+// Prototype function
+Book.prototype.info = function info() {
+  return `${this.title  } by ${  this.author  }, ${  this.pages  }, ${  this.read}`;
 }
 
-const theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', '295 pages', 'not read yet')
-
-console.log(theHobbit.info());
-
-//Book addition function
+// Book addition function
 function addBookToLibrary() {
-  //function
+  const title = prompt('enter title', 'title');
+  const author = prompt('enter author', 'author');
+  const pages = prompt('enter num of pages', '0');
+  const status = prompt('have you read it?', 'yes/no');
+  const newBook = new Book(title, author, pages, `read: ${status}`);
+
+  myLibrary.push(newBook);
+
+  console.log(newBook.info());
+  console.log(myLibrary);
 }
+
+document.getElementById("btn-add").addEventListener("click", addBookToLibrary);
