cbb297183309282e5edcdb951aa2c460ef65a04d
Javier <javier.medina15@gmail.com>
2023-02-18 19:54:15 -0500

    Write a function that loops through the array

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
-// Library
+// Library array
 let myLibrary = [];
 
-console.log(myLibrary);
-
-// Constructor
+// Constructor function
 function Book(title, author, pages, read) {
   this.title = title
   this.author = author
@@ -16,6 +14,55 @@ Book.prototype.info = function info() {
   return `${this.title  } by ${  this.author  }, ${  this.pages  }, ${  this.read}`;
 }
 
+// Demo books
+const harryPotter = new Book('Harry Potter', "J.K. Rowling", "300p", `read: yes`);
+myLibrary.push(harryPotter);
+const lordOfTheRings = new Book('Lord of the rings', "Some fantasy author", "500p", `read: no`);
+myLibrary.push(lordOfTheRings);
+const sevenHabits = new Book('7 Habits of effective people', "Some smart author", "100p", `read: yes`);
+myLibrary.push(sevenHabits);
+
+// Create book card function
+function createBookCard(book) {
+  const container = document.querySelector('#book-container');
+
+  const content = document.createElement('div');
+  content.classList.add('book-card');
+  content.setAttribute('id', `book-card${myLibrary.indexOf(book)}`);
+  
+  container.appendChild(content);
+}
+
+// Display book info function
+function displayBookInfo(book) {
+  const container = document.querySelector(`#book-card${myLibrary.indexOf(book)}`);
+  
+  const bookTitle = document.createElement('p');
+  bookTitle.textContent = book.title;
+  
+  const bookAuthor = document.createElement('p');
+  bookAuthor.textContent = book.author;
+  
+  const bookPages = document.createElement('p');
+  bookPages.textContent = book.pages;
+  
+  const bookStatus = document.createElement('p');
+  bookStatus.textContent = book.read;
+
+  container.appendChild(bookTitle);
+  container.appendChild(bookAuthor);
+  container.appendChild(bookPages);
+  container.appendChild(bookStatus);
+}
+
+// Loop through book library function
+function loopBookLibrary() {
+  myLibrary.forEach(book => createBookCard(book));
+  myLibrary.forEach(book => displayBookInfo(book));
+}
+
+loopBookLibrary();
+
 // Book addition function
 function addBookToLibrary() {
   const title = prompt('enter title', 'title');
@@ -23,11 +70,10 @@ function addBookToLibrary() {
   const pages = prompt('enter num of pages', '0');
   const status = prompt('have you read it?', 'yes/no');
   const newBook = new Book(title, author, pages, `read: ${status}`);
-
-  myLibrary.push(newBook);
-
+  
   console.log(newBook.info());
-  console.log(myLibrary);
+  
+  myLibrary.push(newBook);
 }
 
 document.getElementById("btn-add").addEventListener("click", addBookToLibrary);
