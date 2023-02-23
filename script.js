72b6f429ad50940c7c963fc3e1516702763e233a
Javier <javier.medina15@gmail.com>
2023-02-23 14:36:26 -0500

    Clean up code

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
-// Library
 const myLibrary = [];
 
-// Constructor and prototype
 class Book {
   constructor(title, author, pages, isRead) {
     this.title = title;
@@ -15,45 +13,46 @@ Book.prototype.readToggle = function readToggle(arrIndex) {
   myLibrary[arrIndex].isRead = !myLibrary[arrIndex].isRead;
 }
 
-Book.prototype.remove = function remove(arrIndex) {
+Book.prototype.removeBook = function removeBook(arrIndex) {
   myLibrary.splice(arrIndex, 1);
 }
 
-// Form fields selectors
-const form = document.querySelector("#form");
-const title = document.querySelector("#title");
-const author = document.querySelector("#author");
-const pages = document.querySelector("#pages");
-const readIt = document.querySelector("#status");
-
 // Demo books (to be deleted when finish styling)
 const harryPotter = new Book(
-  "Harry Potter and the Sorcerer's Stone", 
-  "J. K. Rowling", 
-  "223p", 
+  "Harry Potter and the Sorcerer's Stone",
+  "J. K. Rowling",
+  "223p",
   true);
 myLibrary.push(harryPotter);
 const lordOfTheRings = new Book(
-  'The Lord of the Rings', 
-  "J. R. R. Tolkien", 
-  "9250p", 
+  'The Lord of the Rings',
+  "J. R. R. Tolkien",
+  "9250p",
   false);
 myLibrary.push(lordOfTheRings);
 const atomicHabits = new Book(
-  'Atomic Habits: An Easy & Proven Way to Build Good Habits & Break Bad Ones', 
-  "James Clear", 
-  "320p", 
+  'Atomic Habits: An Easy & Proven Way to Build Good Habits & Break Bad Ones',
+  "James Clear",
+  "320p",
   false);
 myLibrary.push(atomicHabits);
 const sevenHabits = new Book(
-  'The 7 Habits of Highly Effective People', 
-  "Stephen Covey", 
-  "381p", 
+  'The 7 Habits of Highly Effective People',
+  "Stephen Covey",
+  "381p",
   true);
 myLibrary.push(sevenHabits);
+// ////////////////////////////////////////////////
 
-// Create book card
-function createBookCard(book) {
+// Form fields selectors
+const form = document.querySelector(".form");
+const formTitle = document.querySelector("#title");
+const formAuthor = document.querySelector("#author");
+const formPages = document.querySelector("#pages");
+const formIsRead = document.querySelector("#isRead");
+
+// Functions
+function createBookCards(book) {
   const container = document.querySelector('#book-container');
 
   const content = document.createElement('div');
@@ -63,39 +62,56 @@ function createBookCard(book) {
   container.appendChild(content);
 }
 
-// Display book info
-function displayBookInfo(book) {
+function addBookTitle(book) {
+  const content = document.createElement('p');
+  content.classList.add('title');
+  content.textContent = book.title;
+  return content;
+}
+
+function addBookAuthor(book) {
+  const content = document.createElement('p');
+  content.classList.add('author');
+  content.textContent = book.author;
+  return content;
+}
+
+function addBookPages(book) {
+  const content = document.createElement('p');
+  content.textContent = book.pages;
+  return content
+}
+
+function addBookIsRead(book) {
+  const content = document.createElement('p');
+  content.textContent = (book.isRead) ? "Read" : "Not read";
+  return content;
+}
+
+function addBookRemoveBtn(book) {
+  const content = document.createElement('button');
+  content.classList.add("btn-remove");
+  content.classList.add(`BR${myLibrary.indexOf(book)}`);
+  content.textContent = 'Remove book';
+  return content;
+}
+
+function addBookReadToggleBtn(book) {
+  const content = document.createElement('button');
+  content.classList.add('btn-status');
+  content.classList.add(`BS${myLibrary.indexOf(book)}`)
+  content.textContent = 'Read status change';
+  return content;
+}
+
+function addBookInfo(book) {
   const container = document.querySelector(`#book-card${myLibrary.indexOf(book)}`);
-  
-  const bookTitle = document.createElement('p');
-  bookTitle.classList.add('title');
-  bookTitle.textContent = book.title;
-  container.appendChild(bookTitle);
-  
-  const bookAuthor = document.createElement('p');
-  bookAuthor.classList.add('author');
-  bookAuthor.textContent = book.author;
-  container.appendChild(bookAuthor);
-  
-  const bookPages = document.createElement('p');
-  bookPages.textContent = book.pages;
-  container.appendChild(bookPages);
-  
-  const bookStatus = document.createElement('p');
-  bookStatus.textContent = (book.isRead) ? "Read" : "Not read";
-  container.appendChild(bookStatus);
-
-  const bookBtn = document.createElement('button');
-  bookBtn.classList.add("btn-remove");
-  bookBtn.classList.add(`BR${myLibrary.indexOf(book)}`);
-  bookBtn.textContent = 'Remove book';
-  container.appendChild(bookBtn);
-
-  const bookStatusBtn = document.createElement('button');
-  bookStatusBtn.classList.add('btn-status');
-  bookStatusBtn.classList.add(`BS${myLibrary.indexOf(book)}`)
-  bookStatusBtn.textContent = 'Read status change';
-  container.appendChild(bookStatusBtn);
+  container.appendChild(addBookTitle(book));
+  container.appendChild(addBookAuthor(book));
+  container.appendChild(addBookPages(book));
+  container.appendChild(addBookIsRead(book));
+  container.appendChild(addBookRemoveBtn(book));
+  container.appendChild(addBookReadToggleBtn(book));
 }
 
 function clearLibrary() {
@@ -105,38 +121,47 @@ function clearLibrary() {
   }
 }
 
-// Loop through book library function
-function loopBookLibrary() {
-  myLibrary.forEach(book => createBookCard(book));
-  myLibrary.forEach(book => displayBookInfo(book));
+function displayLibrary() {
+  form.style.display = "none";
+  clearLibrary();
+  myLibrary.forEach(book => createBookCards(book));
+  myLibrary.forEach(book => addBookInfo(book));
 }
 
-// Buttons clicks handler & listener
+function getArrayIndex(element) {
+  return element.slice(2, element.length);
+}
+
+function getFormStyle(clickClass) {
+  return (clickClass === "btn-open") ? "flex" : "none";
+}
+
+function submitBook() {
+  const newBook = new Book(
+    formTitle.value,
+    formAuthor.value,
+    formPages.value,
+    formIsRead.checked
+  );
+  myLibrary.push(newBook);
+  displayLibrary();
+}
+
 function clickListener(e) {
   const clickClass = e.target.classList;
 
   if (clickClass.contains("btn-remove")) {
-    const element = clickClass[1];
-    const arrIndex = element.slice(2, element.length);
-    myLibrary[arrIndex].remove(arrIndex);
-    clearLibrary();
-    loopBookLibrary();
+    const arrIndex = getArrayIndex(clickClass[1]);
+    myLibrary[arrIndex].removeBook(arrIndex);
+    displayLibrary();
   } else if (clickClass.contains("btn-status")) {
-    const element = clickClass[1];
-    const arrIndex = element.slice(2, element.length);
+    const arrIndex = getArrayIndex(clickClass[1]);
     myLibrary[arrIndex].readToggle(arrIndex);
-    clearLibrary();
-    loopBookLibrary();
-  } else if (clickClass.contains("btn-open")) {
-    form.style.display = "flex";
-  } else if (clickClass.contains("btn-close")) {
-    form.style.display = "none";
+    displayLibrary();
+  } else if (clickClass.contains("btn-open") || (clickClass.contains("btn-close"))) {
+    form.style.display = getFormStyle(clickClass[0]);
   } else if (clickClass.contains(("btn-submit"))) {
-    const newBook = new Book(title.value, author.value, pages.value, readIt.checked);
-    myLibrary.push(newBook);
-    form.style.display = "none";
-    clearLibrary();
-    loopBookLibrary();
+    submitBook();
   }
 }
 
