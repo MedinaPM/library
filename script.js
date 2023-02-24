74420168629e36c6af6d1643d195076798f461d8
Javier <javier.medina15@gmail.com>
2023-02-23 22:19:59 -0500

    Clean up code with Prettier

--- script.js
+++ script.js
@@ -11,33 +11,37 @@ class Book {
 
 Book.prototype.readToggle = function readToggle(arrIndex) {
   myLibrary[arrIndex].isRead = !myLibrary[arrIndex].isRead;
-}
+};
 
 Book.prototype.removeBook = function removeBook(arrIndex) {
   myLibrary.splice(arrIndex, 1);
-}
+};
 
 // Demo books (to be deleted when finish styling)
 const harryPotter = new Book(
   "Harry Potter and the Sorcerer's Stone",
   "J. K. Rowling",
   "223p",
-  true);
+  true
+);
 const lordOfTheRings = new Book(
-  'The Lord of the Rings',
+  "The Lord of the Rings",
   "J. R. R. Tolkien",
   "9250p",
-  false);
+  false
+);
 const atomicHabits = new Book(
-  'Atomic Habits: An Easy & Proven Way to Build Good Habits & Break Bad Ones',
+  "Atomic Habits: An Easy & Proven Way to Build Good Habits & Break Bad Ones",
   "James Clear",
   "320p",
-  false);
+  false
+);
 const sevenHabits = new Book(
-  'The 7 Habits of Highly Effective People',
+  "The 7 Habits of Highly Effective People",
   "Stephen Covey",
   "381p",
-  true);
+  true
+);
 myLibrary.push(harryPotter);
 myLibrary.push(lordOfTheRings);
 myLibrary.push(atomicHabits);
@@ -53,38 +57,38 @@ const formIsRead = document.querySelector("#isRead");
 
 // Functions
 function createBookCards(book) {
-  const container = document.querySelector('#book-container');
-  const content = document.createElement('div');
-  content.classList.add('book-card');
-  content.setAttribute('id', `book-card${myLibrary.indexOf(book)}`);
+  const container = document.querySelector("#book-container");
+  const content = document.createElement("div");
+  content.classList.add("book-card");
+  content.setAttribute("id", `book-card${myLibrary.indexOf(book)}`);
   container.appendChild(content);
 }
 
 function addBookBasicInfo(info) {
-  const content = document.createElement('p');
+  const content = document.createElement("p");
   content.textContent = info;
-  return content
+  return content;
 }
 
 function addBookIsRead(book) {
-  const content = document.createElement('p');
-  content.textContent = (book.isRead) ? "Read" : "Not read";
+  const content = document.createElement("p");
+  content.textContent = book.isRead ? "Read" : "Not read";
   return content;
 }
 
 function addBookRemoveBtn(bIndex) {
-  const content = document.createElement('button');
+  const content = document.createElement("button");
   content.classList.add("btn-remove");
   content.classList.add(`BR${bIndex}`);
-  content.textContent = 'Remove book';
+  content.textContent = "Remove book";
   return content;
 }
 
 function addBookReadToggleBtn(bIndex) {
-  const content = document.createElement('button');
-  content.classList.add('btn-status');
+  const content = document.createElement("button");
+  content.classList.add("btn-status");
   content.classList.add(`BS${bIndex}`);
-  content.textContent = 'Read status change';
+  content.textContent = "Read status change";
   return content;
 }
 
@@ -110,8 +114,8 @@ function clearLibrary() {
 function displayLibrary() {
   form.style.display = "none";
   clearLibrary();
-  myLibrary.forEach(book => createBookCards(book));
-  myLibrary.forEach(book => addBookInfo(book));
+  myLibrary.forEach((book) => createBookCards(book));
+  myLibrary.forEach((book) => addBookInfo(book));
 }
 
 function getArrayIndex(element) {
@@ -119,7 +123,7 @@ function getArrayIndex(element) {
 }
 
 function getFormStyle(clickClass) {
-  return (clickClass === "btn-open") ? "flex" : "none";
+  return clickClass === "btn-open" ? "flex" : "none";
 }
 
 function submitBook() {
@@ -144,9 +148,12 @@ function clickListener(e) {
     const arrIndex = getArrayIndex(clickClass[1]);
     myLibrary[arrIndex].readToggle(arrIndex);
     displayLibrary();
-  } else if (clickClass.contains("btn-open") || (clickClass.contains("btn-close"))) {
+  } else if (
+    clickClass.contains("btn-open") ||
+    clickClass.contains("btn-close")
+  ) {
     form.style.display = getFormStyle(clickClass[0]);
-  } else if (clickClass.contains(("btn-submit"))) {
+  } else if (clickClass.contains("btn-submit")) {
     submitBook();
   }
 }
